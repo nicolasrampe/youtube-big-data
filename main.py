e6d4600c808289e2ed760c2a28615e738d006389
colac <nicolasrampe@unisabana.edu.co>
2022-04-25 21:09:59 -0500

    methods added

--- main.py
+++ main.py
@@ -1,28 +1,43 @@
 # API client library
-import requests
-import os
 import json
-import numpy as np
-import pandas as pd
-import tweepy
-import googleapiclient.discovery
+
+import googleapiclient.discovery as youtube
+import pathlib
+
 # API information
 api_service_name = "youtube"
 api_version = "v3"
 DEVELOPER_KEY = 'AIzaSyBYwVqhlTzox2YdwHMk9ZKRQJkNkj2F2N0'
 # API client
-youtube = googleapiclient.discovery.build(
-        api_service_name, api_version, developerKey = DEVELOPER_KEY)
-# Request body
-request = youtube.search().list(
+youtube = youtube.build(
+    api_service_name, api_version, developerKey=DEVELOPER_KEY)
+
+def get_comments(video_id):
+  results = youtube.commentThreads().list(
+    part="snippet",
+    videoId=video_id,
+    textFormat="plainText"
+  ).execute()
+def createjson_Videos(regionCode, keyWord):
+    request = youtube.search().list(
         part="id,snippet",
-        regionCode= 'CO',
+        regionCode=regionCode,
         type='video',
-        q="Energías Renovables",
+        q=keyWord,
         videoDuration='short',
         videoDefinition='high',
         maxResults=50
-)
-# Request execution
-response = request.execute()
-print(response)
+    )
+    # Request execution
+    search_response = request.execute()
+    for search_result in search_response.get("items", []):
+        get_comments(search_result["id"]["videoId"])
+
+
+    print(response)
+    with open("./json/" + regionCode + "Videos.json", "w") as outfile:
+        json.dump(response, outfile)
+
+
+# Request body
+createjson_Videos()
