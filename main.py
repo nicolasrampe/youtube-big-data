4ceaefd487b18c369435e8c97eada7a3c5cbc808
colac <nicolasrampe@unisabana.edu.co>
2022-04-28 21:39:20 -0500

    Convetir a CSV

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 # API client library
 import json
 import tkinter as tk
+import csv
 import pandas as pd
-from os import path
-from PIL import Image
-from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
+import matplotlib.pyplot as plt
+from wordcloud import WordCloud
 
 from tkinter import ttk
 from tkinter import *
+from tkinter import messagebox
 import googleapiclient.discovery as youtube
 import pathlib
 
@@ -18,7 +19,7 @@ DEVELOPER_KEY = 'AIzaSyBYwVqhlTzox2YdwHMk9ZKRQJkNkj2F2N0'  # Generada en Google 
 # API client
 youtube = youtube.build(
     api_service_name, api_version, developerKey=DEVELOPER_KEY)  # Construir objeto tipo googleapiclient.discovery
-
+row = 0
 
 def get_comments(video_id):
     request = youtube.commentThreads().list(
@@ -28,9 +29,17 @@ def get_comments(video_id):
     try:  # Hay vídeos que tienen bloqueados los comentarios
         search_response = request.execute()
         print(search_response)
+        data_file = open('.\csv\jsonoutput.csv', 'w', newline='')
+        csv_writer = csv.writer(data_file)
         for search_result in search_response.get("items", []):
             with open("./json/comments/" + search_result["id"] + "_Videos.json", "w") as outfile:
                 json.dump(search_response, outfile)
+            for data in outfile:
+                if row == 0:
+                    header = data.keys()
+                    csv_writer.writerow(header)
+                    row += 1
+                csv_writer.writerow(data.values())
     except Exception:
         print("Oops! ")
 
@@ -47,11 +56,10 @@ def createWordsCloud():
     plt.axis("off")
     plt.show()
 
-
 def createjson_Videos():
     request = youtube.search().list(
         part="id,snippet",
-        regionCode='CO',
+        regionCode=drop_down_region_code.get(),
         type='video',
         q=search_box_palabra_clave.get(),
         videoDuration='short',
@@ -63,15 +71,17 @@ def createjson_Videos():
     for search_result in search_response.get("items", []):
         get_comments(search_result["id"]["videoId"])
     print(search_response)
-    with open("./json/videos/" + search_box_palabra_clave.get() + " Videos.json", "w") as outfile:
+    with open("./json/videos/"+ drop_down_region_code.get() + "_" + search_box_palabra_clave.get() + "_Videos.json", "w") as outfile:
         json.dump(search_response, outfile)
     createWordsCloud()
 
 
-
+def on_close():
+    if messagebox.askokcancel("Quit", "Do you want to quit?"):
+        ventana.destroy()
 
 ventana = tk.Tk()
-ventana.title("Conversor de temperatura")
+ventana.title("Buscador de comentarios")
 ventana.config(width=400, height=300)
 
 etiqueta_palabra_clave = ttk.Label(text="Palabra clave:")
@@ -89,10 +99,10 @@ OPTIONS = [
 ] #etc
 
 
-variable = StringVar(ventana)
-variable.set(OPTIONS[0]) # default value
+drop_down_region_code = StringVar(ventana)
+drop_down_region_code.set(OPTIONS[0]) # default value
 
-w = OptionMenu(ventana, variable, *OPTIONS)
+w = OptionMenu(ventana, drop_down_region_code, *OPTIONS)
 w.place(x=240, y=15, width=60)
 
 boton_convertir = ttk.Button(text="Buscar comentarios", command=createjson_Videos)
@@ -101,4 +111,8 @@ etiqueta_temp_kelvin = ttk.Label(text="Temperatura en K: n/a")
 etiqueta_temp_kelvin.place(x=20, y=120)
 etiqueta_temp_fahrenheit = ttk.Label(text="Temperatura en ºF: n/a")
 etiqueta_temp_fahrenheit.place(x=20, y=160)
+
+ventana.protocol("WM_DELETE_WINDOW", on_close)
+
+
 ventana.mainloop()
